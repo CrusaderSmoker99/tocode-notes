87a08170a0c0c83e62252ab7c5546af7df7692da
Crusader99 <smoker68@mail.ru>
2024-11-25 15:37:55 +0300

    vue cli

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wrapper">
+        <Header/>
+        <div class="wrapper-content">
+            <h1>Привет</h1>
+            <p>Большой привет</p>
+            <div class="container">
+                <router-view/>
+            </div>
+        </div>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+    components:{Header,Footer},
+};
+</script>
+
+<style lang="scss"></style>
+
